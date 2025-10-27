51ac683915c30a2daeafbd06b14143e10207ee29
onefatir <me@onefatir.dev>
2025-10-27 15:42:03 +0700

    feat: add webmanifest and update index page metadata for Sladder Club

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,22 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Phaser Nextjs Template</title>
-                <meta name="description" content="A Phaser 3 Next.js project template that demonstrates Next.js with React communication and uses Vite for bundling." />
+                <title>Sladder Club</title>
+                <meta name="description" content="A modern, interactive 'Snake and Ladders' inspired board game built with PhaserJS and Next.js. Experience the classic childhood game with beautiful custom artwork, smooth animations, and engaging gameplay mechanics." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.png" />
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="theme-color" content="#EAC7A3"></meta>
+
+                <meta property="og:image:alt" content="Sladder Club - A modern Snake and Ladders inspired board game"></meta>
+                <meta property="og:image:type" content="image/png"></meta>
+                <meta property="og:image:width" content="1920"></meta>
+                <meta property="og:image:height" content="1080"></meta>
+                <meta property="og:image" content="https://sladder.onefatir.dev/opengraph-image.png"></meta>
+                <meta property="og:title" content="Sladder Club"></meta>
+                <meta property="og:description" content="A modern, interactive 'Snake and Ladders' inspired board game built with PhaserJS and Next.js. Experience the classic childhood game with beautiful custom artwork, smooth animations, and engaging gameplay mechanics."></meta>
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:url" content="https://sladder.onefatir.dev"></meta>
+                <meta property="og:site_name" content="Sladder Club"></meta>
             </Head>
             <main className={`${styles.main} ${pixelify.className}`}>
                 <AppWithoutSSR />
